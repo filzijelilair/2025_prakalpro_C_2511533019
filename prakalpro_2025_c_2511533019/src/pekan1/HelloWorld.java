69791361346010ec3fff016da509f5c82c2345a6
filzijelilair <filzijelila0106@gmail.com>
2025-09-09 12:18:58 +0700

    Praktikum1KelasC

---
+++ prakalpro_2025_c_2511533019/src/pekan1/HelloWorld.java
@@ -0,0 +1,20 @@
+package pekan1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// program pertama
+		/*komentar
+		 * dua baris atau lebih
+		 * pertemuan 1 praktikum alpro
+		 */
+System.out.println("Hello World");
+System.out.println();
+System.out.println("program ini menghasilkan");
+System.out.println("empat baris output");
+System.out.println("/ \\ // \\\\ /// \\\\");
+System.out.println("this"+" program prints a\n"+ "quote from the Gettyburg Address.");
+	}
+
+}
+  
