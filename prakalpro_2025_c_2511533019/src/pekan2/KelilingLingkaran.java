113fbba35d2dbb0992b9e6418637c591712f1b38
filzijelilair <filzijelila0106@gmail.com>
2025-09-16 11:53:29 +0700

    Add files via upload
    praktikum pekan2 kelas C

---
+++ prakalpro_2025_c_2511533019/src/pekan2/KelilingLingkaran.java
@@ -0,0 +1,10 @@
+package pekan2;
+
+public class KelilingLingkaran {
+	public static void main (String[] args) {
+		final double PI = 3.14; /*definisi konstanta*/
+		double radius = 25; /*deklarasi variabel*/
+			System.out.println("Keliling=" +2*PI*radius);
+	}
+
+}
