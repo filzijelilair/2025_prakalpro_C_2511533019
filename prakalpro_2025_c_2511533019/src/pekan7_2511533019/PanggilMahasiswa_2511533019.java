621551fde8e47aa36e40b4af94d0915be43d1496
filzijelilair <filzijelila0106@gmail.com>
2025-11-11 12:04:30 +0700

    praktikum alpro pekan 7_2511533019

---
+++ prakalpro_2025_c_2511533019/src/pekan7_2511533019/PanggilMahasiswa_2511533019.java
@@ -0,0 +1,15 @@
+package pekan7_2511533019;
+
+public class PanggilMahasiswa_2511533019 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mahasiswa_2511533019 a= new Mahasiswa_2511533019();
+		a.setNim (25532);
+		a.setNama ("jely");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+	}
+
+}
---
+++ prakalpro_2025_c_2511533019/src/pekan7_2511533019/String1_2511533019.java
@@ -0,0 +1,16 @@
+package pekan7_2511533019;
+
+public class String1_2511533019 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String salam= "Assalamualaikum";
+		System.out.println("panjang salam adalah : "+salam.length());
+		System.out.println(salam.toUpperCase()); //Outputs "ASSALAMUALAIKUM"
+		System.out.println(salam.toLowerCase()); //outputsnya ga kapital
+		System.out.println(salam.indexOf("salam")); //outputs 2
+		
+		
+	}
+
+}
